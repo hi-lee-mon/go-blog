bbb8dc4438f7a46bc719e9726f02edef7ab5d966
hi-lee-mon <shunmel.09@gmail.com>
2026-05-04 16:25:10 +0900

    ♻️ ifをswitchに変更

--- parser/parser.go
+++ parser/parser.go
@@ -6,10 +6,10 @@ import (
 )
 
 const (
-	PrefixHeadingOne   = "# "
-	PrefixHeadingTwo   = "## "
-	PrefixHeadingThree = "### "
-	PrefixBulletPoint  = "- "
+	prefixHeadingOne   = "# "
+	prefixHeadingTwo   = "## "
+	prefixHeadingThree = "### "
+	prefixBulletPoint  = "- "
 )
 
 func replaceStrong(input string) string {
@@ -33,23 +33,33 @@ func writeString(input string, b *strings.Builder) {
 }
 
 func Parse(input string) string {
+	// HTMLビルダー
 	var b strings.Builder
 
-	// 状態
+	// リスト要素 状態
 	inBulletPoint := false
 	var bulletPointBuilder strings.Builder
 
-	// パース処理
+	/*
+		パース処理
+	*/
 	lines := strings.Split(input, "\n")
 	for _, line := range lines {
-		// 状態毎の処理
+		/*
+			状態毎の処理
+		*/
 		if inBulletPoint {
-			if strings.HasPrefix(line, PrefixBulletPoint) {
-				replacedLine := strings.Replace(line, PrefixBulletPoint, "", 1)
+			/*
+				liビルド処理
+			*/
+			if strings.HasPrefix(line, prefixBulletPoint) {
+				// 連続したli処理
+				replacedLine := strings.Replace(line, prefixBulletPoint, "", 1)
 				content := fmt.Sprintf("<li>%s</li>", replacedLine)
 				bulletPointBuilder.WriteString(content)
 				continue
 			} else {
+				// liの次にli以外が来た場合はulで閉じる
 				contents := bulletPointBuilder.String()
 				content := fmt.Sprintf("<ul>%s</ul>", contents)
 				writeString(content, &b)
@@ -59,31 +69,39 @@ func Parse(input string) string {
 			}
 		}
 
-		// 状態なし処理
-		if strings.HasPrefix(line, PrefixHeadingOne) {
-			replacedLine := strings.Replace(line, PrefixHeadingOne, "", 1)
+		/*
+			状態なしの処理
+		*/
+		switch {
+		case strings.HasPrefix(line, prefixHeadingOne):
+			replacedLine := strings.Replace(line, prefixHeadingOne, "", 1)
 			content := fmt.Sprintf("<h1>%s</h1>", replacedLine)
 			writeString(content, &b)
-		} else if strings.HasPrefix(line, PrefixHeadingTwo) {
-			replacedLine := strings.Replace(line, PrefixHeadingTwo, "", 1)
+		case strings.HasPrefix(line, prefixHeadingTwo):
+			replacedLine := strings.Replace(line, prefixHeadingTwo, "", 1)
 			content := fmt.Sprintf("<h2>%s</h2>", replacedLine)
 			writeString(content, &b)
-		} else if strings.HasPrefix(line, PrefixHeadingThree) {
-			replacedLine := strings.Replace(line, PrefixHeadingThree, "", 1)
+		case strings.HasPrefix(line, prefixHeadingThree):
+			replacedLine := strings.Replace(line, prefixHeadingThree, "", 1)
 			content := fmt.Sprintf("<h3>%s</h3>", replacedLine)
 			writeString(content, &b)
-		} else if strings.HasPrefix(line, PrefixBulletPoint) {
-			replacedLine := strings.Replace(line, PrefixBulletPoint, "", 1)
+
+		case strings.HasPrefix(line, prefixBulletPoint):
+			replacedLine := strings.Replace(line, prefixBulletPoint, "", 1)
 			content := fmt.Sprintf("<li>%s</li>", replacedLine)
 			bulletPointBuilder.WriteString(content)
 			inBulletPoint = true
 			continue
-		} else {
+		default:
 			content := fmt.Sprintf("<p>%s</p>", line)
 			writeString(content, &b)
 		}
 	}
 
+	/*
+		入れ子解決処理
+		最終行が<li>担っている場合<ul>で閉じる必要がある。それを解決するための処理
+	*/
 	if inBulletPoint {
 		contents := bulletPointBuilder.String()
 		content := fmt.Sprintf("<ul>%s</ul>", contents)
